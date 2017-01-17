648aaea1522d2a371b114d063caf394db540a79f
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-01-17 22:56:54 +0100

    :package: Import base

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// var $ = require('./libs/jquery/dist/jquery.slim.min.js');
+var $ = require('jquery');
+
+var TweenMax = require('gsap');
+
+
+$(function(){
+
+    window.requestAnimFrame = require('./requestAnimFrame.js');
+    var throttle = require('./throttle.js');
+
+    var body = $('body');
+    var windowWidth = $(window).outerWidth(), windowHeight = $(window).height();
+
+
+
+    // isMobile.any ? body.addClass('is-mobile') : body.addClass('is-desktop');
+
+    function resizeHandler(){
+        windowWidth = $(window).outerWidth();
+        windowHeight = $(window).height();
+    }
+
+    $(window).on('resize', throttle(
+        requestAnimFrame(resizeHandler), 60)
+    ).on('load', function(){
+
+    });
+
+
+    $(document).on('scroll', throttle(function(){
+
+    }, 60));
+
+});
