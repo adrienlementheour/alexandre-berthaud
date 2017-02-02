ee60b9e543aa27a8b9273826c81aa3718298ab4f
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-02-02 22:11:44 +0100

    :art: Resize bg

--- src/js/bgPage.js
+++ src/js/bgPage.js
@@ -1,13 +1,29 @@
 var $ = require('jquery');
 import TweenMax from 'gsap';
 
+window.requestAnimFrame = require('./requestAnimFrame.js');
+var throttle = require('./throttle.js');
+
 module.exports = function(){
 	var bg = $('.bg-page'),
-		containerWidth, windowWidth, bgWidth;
+		containerWidth, windowWidth, bgWidth,
+		windowHeight = $(window).height(), windowWidth = $(window).width();
 	
-	windowWidth = $(window).outerWidth();
-	containerWidth = $('.container').outerWidth();
-	bgWidth = ((windowWidth-containerWidth)/2) + ((containerWidth*16.666667)/100) + 13;
+	function setBgWidth(){
+		windowWidth = $(window).outerWidth();
+		containerWidth = $('.container').outerWidth();
+		bgWidth = ((windowWidth-containerWidth)/2) + ((containerWidth*16.666667)/100) + 13;
 
-	TweenMax.set(bg, {width: bgWidth+'px'});
+		TweenMax.set(bg, {width: bgWidth+'px'});
+	}
+	setBgWidth();
+
+	$(window).on('resize', throttle(function(){
+    	var nh = $(window).height(), nw = $(window).width();
+    	if (nw != windowWidth){
+			setBgWidth();
+		}
+		windowHeight = nh;
+		windowWidth = nw;
+	}, 60));
 }
