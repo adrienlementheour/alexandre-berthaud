94cea8658e9884dcdb8b0854aabe58a71098365e
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-02-02 22:09:34 +0100

    :art: Slider resize

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,6 +3,9 @@ import TweenMax from 'gsap';
 import Draggable from 'gsap/Draggable';
 import ThrowPropsPlugin from 'gsap/plugins/ThrowPropsPlugin.js';
 
+window.requestAnimFrame = require('./requestAnimFrame.js');
+var throttle = require('./throttle.js');
+
 module.exports = function(){
 	var wrapperSliders = $('.wrapper-sliders'), wrapperSlider = $('.wrapper-slider'), sliderList, sliderElem,
 		wrapperTitles = $('.wrapper-projects-titles'), titlesList, titlesElem, nbTitles, widthWrapperTitles,
@@ -11,19 +14,42 @@ module.exports = function(){
 		sliderTarget, wrapperSliderTarget, widthWrapperTarget, xPositionTarget, wrappersSlidersSiblings, widthSliderSiblings,
 		wrapperOther = $('.wrapper-projects-other'),
 		elemSlider = $('.elem-slider'), elemList, elemPart, elemPartWidth, tlElems,
-		goingLeft = true, nbElems, nbElemsIn, currentNb, initialTimeoutSlides, timeoutSlides;
+		goingLeft = true, nbElems, nbElemsIn, currentNb, initialTimeoutSlides, timeoutSlides,
+		windowHeight = $(window).height(), windowWidth = $(window).width();
 
+
+	function otherInit(){
+		wrapperOther.each(function(){
+			titlesList = $(this).find('ul');
+			titlesElem = titlesList.find('li');
+			nbTitles = titlesElem.length;
+			TweenMax.set(titlesList, {clearProps: 'width'});
+			TweenMax.set(titlesElem, {clearProps: 'width'});
+			if($(this).hasClass('wrapper-projects-titles')){
+				widthWrapperTitles = titlesList.outerHeight();
+			}else{
+				widthWrapperTitles = titlesList.outerWidth();
+			}
+			titlesList.data('width', widthWrapperTitles).data('slide-width', widthWrapperTitles);
+			TweenMax.set(titlesList, {width: nbTitles*widthWrapperTitles+'px'});
+			TweenMax.set(titlesElem, {width: widthWrapperTitles+'px'});
+			titlesElem.each(function(i){
+				TweenMax.set($(this), {left: i*widthWrapperTitles+'px'});
+			});
+		});
+	}
+
+	function reinitSlidesPositions(){
+		elemSlider.each(function(index){
+			elemList = $(this).find('ul');
+			TweenMax.set(elemList, {x: 0});
+		});
+	}
 
 	function moveSlides(goingLeft){
 		elemSlider.each(function(index){
 			elemList = $(this).find('ul');
-			elemPart = elemList.find('li');
-			if($(this).hasClass('wrapper-projects-titles')){
-				// rotated element
-				elemPartWidth = elemPart.outerHeight();
-			}else{
-				elemPartWidth = elemPart.outerWidth();
-			}
+			elemPartWidth = elemList.data('slide-width');
 			if(goingLeft){
 				TweenMax.to(elemList, 0.5, {x: '-='+elemPartWidth, ease: Power3.easeInOut});
 			}else{
@@ -85,7 +111,10 @@ module.exports = function(){
 		sliderList = $(this).find('ul');
 		sliderElem = sliderList.find('li');
 		nbSlides = sliderElem.length;
+		TweenMax.set(sliderList, {clearProps: 'width'});
+		TweenMax.set(sliderElem, {clearProps: 'width'});
 		widthWrapper[indexSlider] = $(this).outerWidth();
+		sliderList.data('width', nbSlides*widthWrapper[indexSlider]).data('slide-width', widthWrapper[indexSlider]);
 		TweenMax.set(sliderList, {width: nbSlides*widthWrapper[indexSlider]+'px'});
 		TweenMax.set(sliderElem, {width: widthWrapper[indexSlider]+'px'});
 		sliderElem.each(function(i){
@@ -112,23 +141,41 @@ module.exports = function(){
 	});
 
 	// Place project titles
-	wrapperOther.each(function(){
-		titlesList = $(this).find('ul');
-		titlesElem = titlesList.find('li');
-		nbTitles = titlesElem.length;
-		if($(this).hasClass('wrapper-projects-titles')){
-			widthWrapperTitles = titlesList.outerHeight();
-		}else{
-			widthWrapperTitles = titlesList.outerWidth();
-		}
-		TweenMax.set(titlesList, {width: nbTitles*widthWrapperTitles+'px'});
-		TweenMax.set(titlesElem, {width: widthWrapperTitles+'px'});
-		titlesElem.each(function(i){
-			TweenMax.set($(this), {left: i*widthWrapperTitles+'px'});
-		});
-	});
+	otherInit();
 	nbElems = elemSlider.length;
 	nbElemsIn = elemSlider.first().find('li').length;
 	currentNb = nbElemsIn;
 	initialTimeoutSlides = setTimeout(animateSlides, 5000);
+
+	$(window).on('resize', throttle(function(){
+    	var nh = $(window).height(), nw = $(window).width();
+    	if (nw != windowWidth){
+			// Si le resize se fait sur la largeur seulement
+			wrapperSlider.each(function(indexSlider){
+				sliderList = $(this).find('ul');
+				sliderElem = sliderList.find('li');
+				nbSlides = sliderElem.length;
+				TweenMax.set(sliderList, {clearProps: 'width'});
+				TweenMax.set(sliderElem, {clearProps: 'width'});
+				widthWrapper[indexSlider] = $(this).outerWidth();
+				sliderList.data('width', nbSlides*widthWrapper[indexSlider]).data('slide-width', widthWrapper[indexSlider]);
+				TweenMax.set(sliderList, {width: nbSlides*widthWrapper[indexSlider]+'px'});
+				TweenMax.set(sliderElem, {width: widthWrapper[indexSlider]+'px'});
+				sliderElem.each(function(i){
+					TweenMax.set($(this), {left: i*widthWrapper[indexSlider]+'px'});
+				});
+			});
+
+			otherInit();
+			reinitSlidesPositions();
+			stopAnimSlider();
+			initialTimeoutSlides = setTimeout(animateSlides, 5000);
+			goingLeft = true;
+			nbElems = elemSlider.length;
+			nbElemsIn = elemSlider.first().find('li').length;
+			currentNb = nbElemsIn;
+		}
+		windowHeight = nh;
+		windowWidth = nw;
+	}, 60));
 }
